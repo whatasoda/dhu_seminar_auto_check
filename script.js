9726df8bd563621dd112d1df35fc73ba351a14f3
whatasoda <a16dc107@dhw.ac.jp>
2019-06-27 16:46:41 +0900

    fix bug

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-;
 (function () {
     var PARSE_LESSON_BLOCK_START = /科目分類\t科目分野\t学則グループ\t学則科目\t教員名\t単位数\t評価\t年度\t開講時期/m;
     var PARSE_LESSON_BLOCK_END = /\t総取得単位数\t/m;
@@ -7,9 +6,10 @@
         var end = input.match(PARSE_LESSON_BLOCK_END);
         if (!(start && start.index && end && end.index))
             return alert('入力形式を確認してください。');
-        var table = input.slice(start.index + start[0].length, end.index).split('\n').map(function (line) {
-            return line.split('\t').map(function (item) { return item.trim(); });
-        });
+        var table = input
+            .slice(start.index + start[0].length, end.index)
+            .split('\n')
+            .map(function (line) { return line.split('\t').map(function (item) { return item.trim(); }); });
         return table
             .map(function (row) { return [row[3] ? fixFormat(row[3]) : '', !!(row[3] && row[6] !== '不可')]; })
             .filter(function (_a) {
@@ -31,11 +31,13 @@
     };
     var __LESSONS_STR__ = '';
     var dist = function () {
+        var toEscape = ['\\', '*', '+', '.', '?', '{', '}', '(', ')', '[', ']', '^', '$', '-', '|', '/'].map(function (c) { return "\\" + c; });
+        var escapeRegexp = new RegExp("(" + toEscape.join('|') + ")", 'g');
         var targets = Array.from(document.querySelectorAll('.inner>label'));
         var targetMapStr = targets.reduce(function (prev, curr, i) { return prev + (";" + i + ":" + curr.textContent.trim() + ";"); }, '');
         var lessons = JSON.parse(__LESSONS_STR__);
         var failed = lessons
-            .map(function (lesson) { return [lesson, new RegExp(";([0-9]*):" + lesson + ".*?;")]; })
+            .map(function (lesson) { return [lesson, new RegExp(";([0-9]*):" + lesson.replace(escapeRegexp, '\\$1') + ".*?;")]; })
             .filter(function (_a) {
             var lesson = _a[0], pattern = _a[1];
             var match = targetMapStr.match(pattern);
@@ -63,10 +65,6 @@
             console.log(failed.join('\n'));
         }
     };
-    var fixFormat = function (lesson) {
-        return lesson
-            .replace(/([a-zⅠⅡⅢⅣ])(?=[A-Z])/g, '$1 ')
-            .replace(/-.*$/, '');
-    };
+    var fixFormat = function (lesson) { return lesson.replace(/([a-zⅠⅡⅢⅣ])(?=[A-Z])/g, '$1 ').replace(/-.*$/, ''); };
     document.getElementById('submit').onclick = apply;
 })();
--- script.ts
+++ script.ts
@@ -1,73 +1,72 @@
-;(() => {
-  const PARSE_LESSON_BLOCK_START = /科目分類\t科目分野\t学則グループ\t学則科目\t教員名\t単位数\t評価\t年度\t開講時期/m
-  const PARSE_LESSON_BLOCK_END = /\t総取得単位数\t/m
+(() => {
+  const PARSE_LESSON_BLOCK_START = /科目分類\t科目分野\t学則グループ\t学則科目\t教員名\t単位数\t評価\t年度\t開講時期/m;
+  const PARSE_LESSON_BLOCK_END = /\t総取得単位数\t/m;
 
-  function parseLessons (input: string) {
-    const start = input.match(PARSE_LESSON_BLOCK_START)
-    const end = input.match(PARSE_LESSON_BLOCK_END)
-    if (!(start && start.index && end && end.index)) return alert('入力形式を確認してください。')
-    
-    const table = input.slice(start.index + start[0].length, end.index).split('\n').map(line =>
-      line.split('\t').map(item => item.trim())
-    )
-    
+  function parseLessons(input: string) {
+    const start = input.match(PARSE_LESSON_BLOCK_START);
+    const end = input.match(PARSE_LESSON_BLOCK_END);
+    if (!(start && start.index && end && end.index)) return alert('入力形式を確認してください。');
+
+    const table = input
+      .slice(start.index + start[0].length, end.index)
+      .split('\n')
+      .map((line) => line.split('\t').map((item) => item.trim()));
+
     return table
-      .map(row => [row[3] ? fixFormat(row[3]): '', !!(row[3] && row[6] !== '不可')] as [string, boolean])
+      .map((row) => [row[3] ? fixFormat(row[3]) : '', !!(row[3] && row[6] !== '不可')] as [string, boolean])
       .filter(([name, isValid]) => isValid)
-      .map(([name]) => name)
+      .map(([name]) => name);
   }
 
   const apply = () => {
-    const input = (document.getElementById('Source') as HTMLTextAreaElement).value
-    if (!input) return alert('ページを更新して再度実行してください。')
-    const lessons = parseLessons(input)
-    
-    const distText = `;(${dist.toString().replace('__LESSONS_STR__', JSON.stringify(JSON.stringify(lessons)))})()`
-    ;(document.getElementById('Dist') as HTMLTextAreaElement).value = distText
-  }
+    const input = (document.getElementById('Source') as HTMLTextAreaElement).value;
+    if (!input) return alert('ページを更新して再度実行してください。');
+    const lessons = parseLessons(input);
 
-  const __LESSONS_STR__ = ''
+    const distText = `;(${dist.toString().replace('__LESSONS_STR__', JSON.stringify(JSON.stringify(lessons)))})()`;
+    (document.getElementById('Dist') as HTMLTextAreaElement).value = distText;
+  };
+
+  const __LESSONS_STR__ = '';
 
   const dist = () => {
-    const targets = Array.from(document.querySelectorAll('.inner>label'))
-    const targetMapStr = targets.reduce((prev,curr, i) => prev + `;${i}:${curr.textContent.trim()};`, '')
-    
-    const lessons = JSON.parse(__LESSONS_STR__)
+    const toEscape = ['\\', '*', '+', '.', '?', '{', '}', '(', ')', '[', ']', '^', '$', '-', '|', '/'].map(
+      (c) => `\\${c}`
+    );
+    const escapeRegexp = new RegExp(`(${toEscape.join('|')})`, 'g');
+    const targets = Array.from(document.querySelectorAll('.inner>label'));
+    const targetMapStr = targets.reduce((prev, curr, i) => prev + `;${i}:${curr.textContent.trim()};`, '');
+
+    const lessons = JSON.parse(__LESSONS_STR__);
     const failed = lessons
-      .map(lesson => [lesson, new RegExp(`;([0-9]*):${lesson}.*?;`)] as [string, RegExp])
+      .map(
+        (lesson) => [lesson, new RegExp(`;([0-9]*):${lesson.replace(escapeRegexp, '\\$1')}.*?;`)] as [string, RegExp]
+      )
       .filter(([lesson, pattern]) => {
-        
-        const match = targetMapStr.match(pattern)
-        if (!match) return true
-        
-        
-        const index = parseInt(match[1])
-        if (isNaN(index)) return true
-        
-        const target = targets[index]
-        if (!target) return true
-        
-        const input = target.querySelector<HTMLInputElement>(':scope>input')
-        if (!input) return true
-        
-        if (!input.checked) input.click()
-        return false
+        const match = targetMapStr.match(pattern);
+        if (!match) return true;
+
+        const index = parseInt(match[1]);
+        if (isNaN(index)) return true;
+
+        const target = targets[index];
+        if (!target) return true;
+
+        const input = target.querySelector<HTMLInputElement>(':scope>input');
+        if (!input) return true;
+
+        if (!input.checked) input.click();
+        return false;
       })
-      .map(([lesson]) => lesson)
-      
+      .map(([lesson]) => lesson);
+
     if (failed.length) {
       alert('自動チェックに失敗した講義があります。');
       console.log(failed.join('\n'));
-      
     }
-  }
+  };
 
+  const fixFormat = (lesson: string): string => lesson.replace(/([a-zⅠⅡⅢⅣ])(?=[A-Z])/g, '$1 ').replace(/-.*$/, '');
 
-  const fixFormat = (lesson: string): string =>
-    lesson
-      .replace(/([a-zⅠⅡⅢⅣ])(?=[A-Z])/g, '$1 ')
-      .replace(/-.*$/, '')
-      
-      
-  document.getElementById('submit').onclick = apply
-})()
+  document.getElementById('submit').onclick = apply;
+})();
