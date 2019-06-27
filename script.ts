c72cec39ad2812db1c09e00d40cab9417d3a0463
whatasoda <a16dc107@dhw.ac.jp>
2019-06-27 14:25:16 +0900

    update

--- script.ts
+++ script.ts
@@ -56,14 +56,9 @@
       .map(([lesson]) => lesson)
       
     if (failed.length) {
-      const form = document.getElementById('myForm')
-      const notif = document.createElement('div')
-      notif.innerHTML = `以下の講義は自動チェックに失敗しました。手動でチェックを入れてください。<br><br>${failed.join('<br>')}`
-      notif.style.fontSize = '30px'
-      notif.style.color = 'red'
-      form.insertBefore(notif.cloneNode(), form.firstChild)
-      form.insertBefore(notif.cloneNode(), form.lastChild)
-      alert('自動チェックに失敗した講義があります。')
+      alert('自動チェックに失敗した講義があります。');
+      console.log(failed.join('\n'));
+      
     }
   }
 
